7939f01e33a7c9ff4160bd74f40416e0883adaf6
Nta30 <164646563+Nta30@users.noreply.github.com>
2024-03-30 01:30:55 +0700

    Add files via upload

---
+++ bai 1.c
@@ -0,0 +1,51 @@
+// Viet ham tinh ucln(a,b), ap dung ham trn de tinh ucln cua 1 day so nguyen
+#include<stdio.h>
+ int gcd(int a,int b);
+ int gcd_arr(int a[], int n);
+ void nhapmang(int a[], int n);
+ int main(){
+ 	int a,b,ucln;
+ 	scanf("%d%d",&a,&b);
+ 	ucln=gcd(a,b);
+ 	printf("%d",ucln);
+ 	char x;
+	printf("\nBan co muon tinh ucln cua 1 day hay khong(y or n): ");
+	scanf(" %c",&x);
+	if(x=='y'){
+		int i,n;
+		printf("So phan tu can nhap:");
+		scanf("%d",&n);
+		int a[n];
+		printf("Moi nhap vao mang:");
+		nhapmang(a,n);
+		ucln=gcd_arr(a,n);
+		printf("%d",ucln);
+	}  
+ }
+ 
+ void nhapmang(int a[], int n){
+ 	int i;
+ 	for(i=0;i<n;i++){
+ 		scanf("%d",&a[i]);
+	 }
+ }
+  
+ int gcd(int a,int b){
+ 	int temp;
+ 	while(b!=0){
+ 			temp=b;
+			b=a%b;
+ 			a=temp;
+		 printf("%d %d\n",a,b); 	 
+	 } 
+	 return a;
+ }
+ 
+ int gcd_arr(int a[], int n){
+ 	int i,result=a[0];
+ 	for(i=1;i<n;i++){
+ 		result=gcd(result,a[i]);
+	 }
+	 return result;
+ }
+ 
